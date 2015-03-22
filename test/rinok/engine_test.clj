b8e8da219e8c6423796237cbfe66b30c890ff42d
Garrett Thornburg <film42@gmail.com>
2015-03-22 10:06:32 -0600

    add new 2 buy and 2 sell order test

--- test/rinok/engine_test.clj
+++ test/rinok/engine_test.clj
@@ -3,7 +3,7 @@
             [rinok.engine :as eng]))
 
 (deftest matching-sells-to-buys
-  (testing "can match two sell orders to two buy orders"
+  (testing "can match three sell orders to three buy orders"
     (let [engine (eng/->MatchingEngine)
           state (atom [])]
       ;; Register event callback
@@ -23,6 +23,23 @@
                      {:buyer 'A, :seller 'C, :price 10.5, :quantity 100}
                      {:buyer 'A, :seller 'D, :price 10.5, :quantity 100}
                      {:buyer 'E, :seller 'D, :price 10.7, :quantity 100}]))))
+
+  (testing "can match two sell orders to two buy orders"
+    (let [engine (eng/->MatchingEngine)
+          state (atom [])]
+      ;; Register event callback
+      (eng/subscribe engine
+                     (fn [t m] (swap! state conj m)))
+
+      ;; Run tests
+      (eng/accept engine (eng/limit-order 'C 10.4 200 :sell))
+      (eng/accept engine (eng/limit-order 'D 10.3 100 :sell))
+      (eng/accept engine (eng/limit-order 'B 10.6 100 :buy))
+      (eng/accept engine (eng/limit-order 'A 10.5 200 :buy))
+
+      ;; Check results
+      (is (= @state [{:buyer 'B, :seller 'D, :price 10.6, :quantity 100}
+                     {:buyer 'A, :seller 'C, :price 10.5, :quantity 200}]))))
 
   (testing "can match a bunch of really large orders"
     (let [engine (eng/->MatchingEngine)
